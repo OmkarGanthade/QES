7d10dba41c3793ce4e581c2d1971443689315e82
OmkarGanthade <104729308+OmkarGanthade@users.noreply.github.com>
2022-05-01 22:33:44 +0530

    Delete ansqtn.js

--- ansqtn.js
+++
@@ -1,7 +0,0 @@
-function openFormb() {
-    document.getElementById("myForm1").style.display = "block";
-  }
-  
-  function closeFormb() {
-    document.getElementById("myForm1").style.display = "none";
-  }
